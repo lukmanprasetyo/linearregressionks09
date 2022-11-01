4a9d74cfad424ff453bd1295c2c6027f35da3286
lukmanprasetyo <70898568+lukmanprasetyo@users.noreply.github.com>
2022-11-01 16:37:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-import sqlite3
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<h1> Hello  World !! Welcome To Cyber Creed.... </h1>"
+    return "<h1> Hello  World !! </h1>"
 
 
 # main driver function
